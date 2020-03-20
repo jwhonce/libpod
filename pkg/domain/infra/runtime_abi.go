88e1aefc2a998a2dbd93218695dfa4930356ce16
Jhon Honce <jhonce@redhat.com>
2020-03-20 10:28:04 -0700

    Wire remote support into podmanV2
    * Update FlagSet()'s
    * Add --remote and --identity flags
    
    Signed-off-by: Jhon Honce <jhonce@redhat.com>

--- pkg/domain/infra/runtime_abi.go
+++ pkg/domain/infra/runtime_abi.go
@@ -12,27 +12,27 @@ import (
 )
 
 // NewContainerEngine factory provides a libpod runtime for container-related operations
-func NewContainerEngine(mode entities.EngineMode, opts entities.EngineOptions) (entities.ContainerEngine, error) {
-	switch mode {
+func NewContainerEngine(opts entities.EngineOptions) (entities.ContainerEngine, error) {
+	switch opts.EngineMode {
 	case entities.ABIMode:
 		r, err := NewLibpodRuntime(opts.FlagSet, opts.Flags)
 		return r, err
 	case entities.TunnelMode:
-		ctx, err := bindings.NewConnection(context.Background(), opts.Uri.String(), opts.Identities...)
+		ctx, err := bindings.NewConnection(context.Background(), opts.Uri, opts.Identities...)
 		return &tunnel.ContainerEngine{ClientCxt: ctx}, err
 	}
-	return nil, fmt.Errorf("runtime mode '%v' is not supported", mode)
+	return nil, fmt.Errorf("runtime mode '%v' is not supported", opts.EngineMode)
 }
 
 // NewContainerEngine factory provides a libpod runtime for image-related operations
-func NewImageEngine(mode entities.EngineMode, opts entities.EngineOptions) (entities.ImageEngine, error) {
-	switch mode {
+func NewImageEngine(opts entities.EngineOptions) (entities.ImageEngine, error) {
+	switch opts.EngineMode {
 	case entities.ABIMode:
 		r, err := NewLibpodImageRuntime(opts.FlagSet, opts.Flags)
 		return r, err
 	case entities.TunnelMode:
-		ctx, err := bindings.NewConnection(context.Background(), opts.Uri.String(), opts.Identities...)
+		ctx, err := bindings.NewConnection(context.Background(), opts.Uri, opts.Identities...)
 		return &tunnel.ImageEngine{ClientCxt: ctx}, err
 	}
-	return nil, fmt.Errorf("runtime mode '%v' is not supported", mode)
+	return nil, fmt.Errorf("runtime mode '%v' is not supported", opts.EngineMode)
 }
--- pkg/domain/infra/runtime_proxy.go
+++ pkg/domain/infra/runtime_proxy.go
@@ -4,6 +4,8 @@ package infra
 
 import (
 	"context"
+
+	"github.com/containers/libpod/libpod"
 	"github.com/containers/libpod/pkg/domain/entities"
 	"github.com/containers/libpod/pkg/domain/infra/abi"
 	flag "github.com/spf13/pflag"
@@ -11,7 +13,7 @@ import (
 
 // ContainerEngine Proxy will be EOL'ed after podmanV2 is separated from libpod repo
 
-func NewLibpodRuntime(flags flag.FlagSet, opts entities.EngineFlags) (entities.ContainerEngine, error) {
+func NewLibpodRuntime(flags *flag.FlagSet, opts entities.EngineFlags) (entities.ContainerEngine, error) {
 	r, err := GetRuntime(context.Background(), flags, opts)
 	if err != nil {
 		return nil, err
